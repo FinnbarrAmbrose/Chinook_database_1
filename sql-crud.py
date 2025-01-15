25eed4dafc23263d0d029e44881e12c26271e620
Finnbarr Ambrose <finnbarr.ambrose121@gmail.com>
2025-01-15 14:41:04 +0000

    Database management practise

---
+++ sql-crud.py
@@ -0,0 +1,43 @@
+from sqlalchemy import (
+    create_engine, Column,  Integer, String
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+db = create_engine("postgresql:///chinook")
+base = declarative_base()
+
+class Programmer(base):
+    __tablename__ = "Programmer"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    gender = Column(String)
+    nationality = Column(String)
+    famous_for = Column(String)
+
+
+# add each instance of our programmers to our session
+session.add(ada_lovelace)
+# session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_berners_lee)
+
+# commit our session to the database
+# session.commit()
+
+
+# query the database to find all Programmers
+programmers = session.query(Programmer)
+for programmer in programmers:
+    print(
+        programmer.id,
+        programmer.first_name + " " + programmer.last_name,
+        programmer.gender,
+        programmer.nationality,
+        programmer.famous_for,
+        sep=" | "
+    )
